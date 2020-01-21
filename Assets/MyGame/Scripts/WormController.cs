9e14deeebf9564c315dc4cc6e0a11fd86a945551
HellLisa <lisahell@nbgum-lhell03-MacBook-Pro.local>
2020-01-21 15:04:10 +0100

    add Script

---
+++ Assets/MyGame/Scripts/WormController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+    public int x;
+    public string text;
+    
+    void Start()
+    {
+        if (x>0) {
+        
+            Debug.Log("x ist größer als 0");
+        
+        }
+
+        else
+
+        {
+            Debug.Log("x ist kleiner als 0");
+
+        }
+
+        if (text == "oidaaaaaaaa") {
+            Debug.Log("do steht oidaaaaaaaa");
+        }
+        else
+        {
+            Debug.Log("Wos isn mid dia");
+        }
+    }
+}
