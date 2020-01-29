c968e3a1c58ada28ff6ac0cff614f04ad0c97e28
HellLisa <lisahell@nbgum-lhell03-MacBook-Pro.local>
2020-01-29 07:59:49 +0100

    change Script

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -3,44 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WormController : MonoBehaviour
-{
-    public int x;
-    public string text;
-    
-    void Start()
-    {
-       /*if (x>0) {
-        
-            Debug.Log("x ist größer als 0");
-        
-        }
-
-        else
-
+{   
+    void Update()
         {
-            Debug.Log("x ist kleiner als 0");
-
+            if(Input.GetKeyDown(KeyCode.E)) 
+            {
+                Debug.Log("Wird aufgerufen!");
+            }
         }
-
-       if (text == "oidaaaaaaaa") {
-           Debug.Log("do steht oidaaaaaaaa");
-        }
-        else
-    {
-            Debug.Log("Wos isn mid dia");
-     } */
-        
-        if (x>10) {
-        
-            Debug.Log("x ist größer als 10");
-        
-        }
-        
-        else
-        
-        {
-            Debug.Log("x ist kleiner gleich 10");
-        }
-        
-    }
 }
