c8f73f46da9322e76d72c5e671bf71f03b68de01
HellLisa <lisahell@nbgum-lhell03-MacBook-Pro.local>
2020-02-18 15:04:43 +0100

    add movement and Bazooka

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -2,13 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class WormController : MonoBehaviour
-{   
+{
+    public Rigidbody rb;
+    public Vector3 jump;
+    public Vector3 move;
+
     void Update()
         {
-            if(Input.GetKeyDown(KeyCode.E)) 
+        if (Input.GetKeyDown(KeyCode.W)) 
             {
-                Debug.Log("Wird aufgerufen!");
+            Debug.Log("W is to jump");
+            rb.AddForce(jump);
             }
+
+           if (Input.GetKeyDown(KeyCode.A))
+            {
+            Debug.Log("A is to move to the left");
+            rb.AddForce(-move);
         }
+
+            if (Input.GetKeyDown(KeyCode.D)){
+            Debug.Log("D is to move to the right");
+            rb.AddForce(move);
+        } 
+
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+            Debug.Log("C is to shoot");
+            }
+
+    }
 }
+
+
