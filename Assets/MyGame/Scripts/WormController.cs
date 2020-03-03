7cce7556c443d984c8fde8e71fc05a53165991ba
HellLisa <lisahell@nbgum-lhell03-MacBook-Pro.local>
2020-03-03 13:15:59 +0100

    add Force functions

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -6,9 +6,16 @@ using UnityEngine;
 
 public class WormController : MonoBehaviour
 {
+
+    public GameObject bazooka;
+    public GameObject canon;
+    public Transform canonPosition;
+    public float shoot = 75;
     public Rigidbody rb;
     public Vector3 jump;
     public Vector3 move;
+    
+
 
     void Update()
         {
@@ -24,14 +31,30 @@ public class WormController : MonoBehaviour
             rb.AddForce(-move);
         }
 
-            if (Input.GetKeyDown(KeyCode.D)){
+            if (Input.GetKeyDown(KeyCode.D))
+            {
             Debug.Log("D is to move to the right");
             rb.AddForce(move);
-        } 
+            } 
 
             if (Input.GetKeyDown(KeyCode.C))
             {
-            Debug.Log("C is to shoot");
+             Debug.Log("C is to shoot");
+             GameObject sphere = Instantiate(canon, canonPosition.position, Quaternion.identity);
+             sphere.GetComponent<Rigidbody>().AddForce(canon.transform.up * shoot);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Debug.Log("Q is to rotate left");
+                canon.transform.Rotate(0, 0, -5, Space.Self);
+            }
+
+        
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("E is to rotate right");
+                canon.transform.Rotate(0, 0, 5, Space.Self);
             }
 
     }
